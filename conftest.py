60eaff5f0ad73b7498aee61546dfac85112d17f0
AlishaMeier <alisha.meyerr@gmail.com>
2022-09-14 04:50:07 +0500

    add configurate

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope='session', autouse=True)
+def window_expect_size():
+    browser.open('https://demoqa.com/webtables')
+    browser.config.window_width = 1260
+    browser.config.window_height = 1000
+    browser.config.hold_browser_open = True
+    yield
+
